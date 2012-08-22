01184521892ed8fa27fb232f2c7d5dba7172cdf6
Slyshkov A.A <agronomm@gmail.com>
2012-08-22 13:37:44 +0300

    Lectures for Oleg

---
+++ first_lessons/src/array/AvergeNumber.java
@@ -0,0 +1,12 @@
+package array;
+public class AvergeNumber {
+	public static void main (String [] args) { 
+		String [] stringsArray = new String[3];
+		stringsArray[0] = "it's 0";
+		stringsArray[1] = "it's 1";
+		stringsArray[2] = "it's 2";         
+		System.out.println(stringsArray[0]);
+		System.out.println(stringsArray[1]);
+		System.out.println(stringsArray[2]);
+	}
+}
