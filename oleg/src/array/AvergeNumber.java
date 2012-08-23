ee0e2b1f7083a8b3c75dca8f23c73248d5bc5b96
AvtoSE <OlegGUA@gmail.com>
2012-08-23 17:42:36 +0300

    исправился

--- oleg/src/array/AvergeNumber.java
+++ oleg/src/array/AvergeNumber.java
@@ -1,10 +1,7 @@
 package array;
 public class AvergeNumber {
-
-//для функции отступ не делается, она пишется от края файла
-	public static void main (String [] args) {
-		String [] stringsArray = new String[3];{//вот это какая-то 
-							//левая скобка
+public static void main (String [] args) {
+		String [] stringsArray = new String[3];
 		stringsArray[0] = "It's 0";
 		stringsArray[1] = "It's 1";
 		stringsArray[2] = "It's 2";
@@ -12,5 +9,4 @@ public class AvergeNumber {
 		System.out.println(stringsArray[1]);
 		System.out.println(stringsArray[2]);
 		}
-	}
 }
