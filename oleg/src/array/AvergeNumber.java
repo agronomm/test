fa8a10d5f924fdfe7f27f6d1525c0eefb28af4c4
AvtoSE <OlegGUA@gmail.com>
2012-08-23 17:21:17 +0300

    массивы урок 1

---
+++ oleg/src/array/AvergeNumber.java
@@ -0,0 +1,13 @@
+package array;
+public class AvergeNumber {
+	public static void main (String [] args) {
+		String [] stringsArray = new String[3];{
+		stringsArray[0] = "It's 0";
+		stringsArray[1] = "It's 1";
+		stringsArray[2] = "It's 2";
+		System.out.println(stringsArray[0]);
+		System.out.println(stringsArray[1]);
+		System.out.println(stringsArray[2]);
+		}
+	}
+}
