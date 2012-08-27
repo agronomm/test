9568768a2a6c360b5af527da8020af4af922849c
AvtoSE <OlegGUA@gmail.com>
2012-08-27 13:28:55 +0300

    Дни недели

---
+++ secondlesson_arrays/Secondlesson.java
@@ -0,0 +1,31 @@
+package secondlesson_arrays;
+public class Secondlesson {
+public static void main (String [] args) {
+	String [] stringsArray;
+	stringsArray = new String[7];
+	stringsArray[0] = "Monday";
+	stringsArray[1] = "Tuesday";
+	stringsArray[2] = "Wednesday";
+	stringsArray[3] = "Thursday";
+	stringsArray[4] = "Friday";
+	stringsArray[5] = "Saturday";
+	stringsArray[6] = "Sunday";
+	System.out.println("Days of week:");
+	System.out.println(stringsArray[6]);
+	System.out.println(stringsArray[0]);
+	System.out.println(stringsArray[1]);
+	System.out.println(stringsArray[2]);
+	System.out.println(stringsArray[3]);
+	System.out.println(stringsArray[4]);
+	System.out.println(stringsArray[5]);
+	System.out.println("Days in Russian week:");
+	System.out.println(stringsArray[0]);
+	System.out.println(stringsArray[1]);
+	System.out.println(stringsArray[2]);
+	System.out.println(stringsArray[3]);
+	System.out.println(stringsArray[4]);
+	System.out.println(stringsArray[5]);
+	System.out.println(stringsArray[6]);
+	}
+}
+
